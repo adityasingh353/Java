691814908bc498ea99b80049ae4752927582bc2d
adityasingh353 <aditya352a@gmail.com>
2026-06-03 21:41:25 +0530

    Added DSA folder

---
+++ DSA/name.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class name {
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter word");
+        String str=sc.nextLine();
+        int constant=0;
+        int vowel=0;
+        String vowels="aeiou";
+        for (char c1:str.toCharArray()) {
+            if("aeiou".indexOf(c1)!=-1){
+                vowel++;
+            }
+            else {
+                constant++;
+            }
+
+        }
+        System.out.printf("Vowels:%d,Constants:%d\n",vowel,constant);
+
+
+
+
+    }
+
+}
---
+++ DSA/palindrome.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String[] args) {
+        int a=1;
+        Scanner sc=new Scanner(System.in);
+        String word =sc.next();
+
+        int word_length=word.length();
+        char[] arr=word.toCharArray();
+        for (int i = 0; i <word_length; i++) {
+            if(arr[i]==arr[word_length-1-i]){
+                continue;
+
+            }
+            else{
+                a=-1;
+                break;
+            }
+
+        }
+        if(a!=-1){
+            System.out.println("palindrome");
+        }
+        else {
+            System.out.println("not palindrome");
+        }
+
+    }
+}
